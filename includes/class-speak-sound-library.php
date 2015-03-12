ff8a96e039c8d1d61319f40a16381ac64206eab7
vince.cimo <vince.cimo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-12 04:59:56 +0000

    tagging version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/speak-sound-library/tags/1.1@1110854 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-speak-sound-library.php
+++ includes/class-speak-sound-library.php
@@ -129,12 +129,6 @@ class Speak_Sound_Library {
          */
         require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/partials/speak-sound-library-sounds-from-folder.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		//require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-speak-sound-library-public.php';
-
         /**
          * The class responsible for creating a post from the uploader callback
          */
@@ -217,10 +211,10 @@ class Speak_Sound_Library {
 	 */
 	private function define_public_hooks() {
 
-		// $plugin_public = new Speak_Sound_Library_Public( $this->get_plugin_name(), $this->get_version() );
+		 $plugin_public = new Speak_Sound_Library_Public( $this->get_plugin_name(), $this->get_version() );
 
-		// $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		// $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		 $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		 $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
 
